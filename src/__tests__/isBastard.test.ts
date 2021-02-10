0b0e4ac68468e0be5744c478dcf2a6a6c87fbc3c
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2021-02-11 00:30:15 +0100

    feat: updated tests :vertical_traffic_light:

--- src/__tests__/isBastard.test.ts
+++ src/__tests__/isBastard.test.ts
@@ -1,13 +1,56 @@
 import isBasterd from '../index';
+import Faker from 'faker';
 
-test('isBastard', () => {
-  expect(
-    isBasterd({
-      name: 'kevin',
-      age: 42,
-      available_for_work: true,
-      favoriteBeer: { name: 'brewdog' },
-      codeLanguages: ['js'],
-    }),
-  ).toBe('you are a true bastard 😈');
+describe('isBasterd', () => {
+  test('isBastard happy flow', () => {
+    expect(
+      isBasterd({
+        name: Faker.name.firstName(),
+        age: 42,
+        available_for_work: true,
+        favoriteBeer: { name: 'brewdog' },
+        codeLanguages: ['js'],
+      }),
+    ).toBe('you are a true bastard 😈');
+  });
+
+  test('isBastard throws error when no args are given', () => {
+    function emptyfunction() {
+      // @ts-ignore
+      isBasterd();
+    }
+    expect(emptyfunction).toThrowError(/you must provide a bastard object/i);
+  });
+
+  test('isBastard throws error when name is not of type string', () => {
+    function emptyfunction() {
+      // @ts-ignore
+      isBasterd({ name: 42 });
+    }
+    expect(emptyfunction).toThrowError(/name is not of type string/i);
+  });
+
+  test('isBastard throws error when no args are given', () => {
+    function emptyfunction() {
+      // @ts-ignore
+      isBasterd({ name: Faker.name.firstName() });
+    }
+    expect(emptyfunction).toThrowError(/age is not of type number/i);
+  });
+
+  test('isBastard throws error when no args are given', () => {
+    function emptyfunction() {
+      // @ts-ignore
+      isBasterd();
+    }
+    expect(emptyfunction).toThrowError(/you must provide a bastard object/i);
+  });
+
+  test('isBastard throws error when no args are given', () => {
+    function emptyfunction() {
+      // @ts-ignore
+      isBasterd();
+    }
+    expect(emptyfunction).toThrowError(/you must provide a bastard object/i);
+  });
 });
